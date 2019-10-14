4f2e20d98e7e349dfb2960a7751d36df19553b64
elymatos <ely.matos@gmail.com>
2019-10-13 22:49:36 -0300

    passando state pro store

--- apps/webtool/modules/annotation/views/main/vue/store.js
+++ apps/webtool/modules/annotation/views/main/vue/store.js
@@ -8,6 +8,9 @@ let store = new Vuex.Store({
         urlLookupFE: '',
         currentFrame: 0,
         currentState: 'paused',
+        objects: [],
+        currentObject: null,
+        currentObjectState: 'none',// creating, created, selected,editingFE, editingBox, stopping, updated
         duration: 0,
         totalFrames: 0,
         framesRange: {},
@@ -19,6 +22,12 @@ let store = new Vuex.Store({
         currentState(state) {
             return state.currentState
         },
+        currentObject(state) {
+            return state.currentObject
+        },
+        currentObjectState(state) {
+            return state.currentObjectState
+        },
         duration(state) {
             return state.duration
         },
@@ -27,11 +36,20 @@ let store = new Vuex.Store({
         },
     },
     mutations: {
+        objects(state, value) {
+            state.objects = value;
+        },
         currentFrame(state, value) {
             state.currentFrame = value;
         },
         currentState(state, value) {
             state.currentState = value;
+        },
+        currentObject(state, value) {
+            state.currentObject = value;
+        },
+        currentObjectState(state, value) {
+            state.currentObjectState = value;
         },
         duration(state, value) {
             state.duration = value;
@@ -53,6 +71,13 @@ let store = new Vuex.Store({
         setDuration(context, duration) {
             context.commit('duration', duration);
             context.commit('totalFrames', duration * 30); // 30fps
+        },
+        setObjects(context, objectsList) {
+            let objects = [];
+            for (object of objectsList) {
+                objects.push(object)
+            }
+            context.commit('objects', objects);
         }
     },
 })
