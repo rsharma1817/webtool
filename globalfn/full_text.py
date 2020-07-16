a5552c5697e085f473555e5bdcce9231684ba9dc
yongzx <zhengxin.yong@minerva.kgi.edu>
2020-07-16 11:49:39 -0300

    update full_text

--- globalfn/full_text.py
+++ globalfn/full_text.py
@@ -7,11 +7,14 @@ package_directory = os.path.dirname(os.path.abspath(__file__))
 def full_text(lang):
     """
     :param lang: language (i.e., de, el, en, fr, hi, jp, pt, se, ur)
-    :return: a list of sentence ids and a list of corresponding texts
+    :return: a list of tuples (id, sentence_string) of the particular language
     """
     pickled_file = f"{package_directory}/full_text_data/ted_{lang}_ids_sents.pkl"
     assert Path(pickled_file).is_file()
 
     ids, sents = pickle.load(open(pickled_file, 'rb'))
     assert len(ids) == len(sents)
-    return ids, sents
+    return list(zip(ids, sents))
+
+
+print(full_text('en'))
