80984c37d333fab617d196905712dcfa202ab7ec
yongzx <zhengxin.yong@minerva.kgi.edu>
2020-07-18 20:32:45 -0300

    update full_text

--- globalfn/full_text.py
+++ globalfn/full_text.py
@@ -1,10 +1,11 @@
 import pickle
 from pathlib import Path
 import os
+from flair.data import Sentence
 
 package_directory = os.path.dirname(os.path.abspath(__file__))
 
-def full_text(lang):
+def full_text(lang, tokenized=False, unzip=False):
     """
     :param lang: language (i.e., de, el, en, fr, hi, jp, pt, se, ur)
     :return: a list of tuples (id, sentence_string) of the particular language
@@ -13,5 +14,23 @@ def full_text(lang):
     assert Path(pickled_file).is_file()
 
     ids, sents = pickle.load(open(pickled_file, 'rb'))
+
+    # fix error
+    if lang == 'en':
+        for i in range(len(ids)):
+            if ids[i] == 1152:
+                sents[i] = "They're the people who come out the top."
+
+            if ids[i] == 1272:
+                sents[i] = "By the way -- we may not see this future, but they will."
+
+    # tokenized
+    if tokenized:
+        for i, sent in enumerate(sents):
+            sents[i] = Sentence(sent, use_tokenizer=True).to_tokenized_string()
+
     assert len(ids) == len(sents)
-    return list(zip(ids, sents))
+    if not unzip:
+        return list(zip(ids, sents))
+    else:
+        return list(ids), list(sents)
